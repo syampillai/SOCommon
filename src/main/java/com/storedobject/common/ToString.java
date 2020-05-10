4ace4acbf6965aacf50cb90397da604fde707623
Syam <syam@engravgroup.com>
2020-05-11 00:28:53 +0400

    Version updated to 2.0.3 Compiled with Java 11. Added ToString interface.

---
+++ src/main/java/com/storedobject/common/ToString.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Syam Pillai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.storedobject.common;
+
+/**
+ * An interface that converts some type of object to a {@link String}.
+ *
+ * @param <T> Type of object.
+ * @author Syam
+ */
+public interface ToString<T> {
+
+    /**
+     * Convert the object instance to a {@link String}.
+     * @param object Object to convert.
+     * @return Default implementation returns {@link StringUtility#toString(Object)}.
+     */
+    default String toString(T object) {
+        return StringUtility.toString(object);
+    }
+}
